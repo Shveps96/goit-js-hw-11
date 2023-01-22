6257733deaf091c32dbf3f81d354e88b38ad6236
Shveps96 <42212039+Shveps96@users.noreply.github.com>
2023-01-22 22:07:23 +0200

    .

--- src/index.js
+++ src/index.js
@@ -16,7 +16,6 @@ const loadMoreBtn = document.querySelector('.load-more');
 
 searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', addImages);
-// window.addEventListener('scroll', infiniteScroll);
 onScrollBtnUp();
 btnUpToTop();
 
@@ -32,6 +31,11 @@ function onSearch(evt) {
       loadMoreBtn.classList.add('hidden');
       onSearchError();
       return
+    } else if (data.totalHits <= 40) {
+      onSearchSuccess(data);
+      createMarkup(data.hits);
+      loadMoreBtn.classList.add('hidden');
+      simplelightbox.refresh();
     } else {
       onSearchSuccess(data);
       createMarkup(data.hits);
@@ -56,53 +60,3 @@ function addImages() {
       })
       .catch(error => console.log(error));
   }
-
-
-
-
-
-
-
-// function infiniteScroll() {
-//   console.log(document.documentElement.scrollTop); //scrolled from top
-//   console.log(window.innerHeight); //visible part of screen
-//   if (
-//     window.scrollY + window.innerHeight >=
-//     document.documentElement.scrollHeight
-//   ) {
-//     addImages();
-//   }
-// }
-
-
-// const btnUp = {
-//   el: document.querySelector('.btn-up'),
-//   show() {
-//     // удалим у кнопки класс btn-up_hide
-//     this.el.classList.remove('btn-up_hide');
-//   },
-//   hide() {
-//     // добавим к кнопке класс btn-up_hide
-//     this.el.classList.add('btn-up_hide');
-//   },
-//   addEventListener() {
-//     // при прокрутке содержимого страницы
-//     window.addEventListener('scroll', () => {
-//       // определяем величину прокрутки
-//       const scrollY = window.scrollY;
-//       // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-//       scrollY > 656 ? this.show() : this.hide();
-//     });
-//     // при нажатии на кнопку .btn-up
-//     document.querySelector('.btn-up').onclick = () => {
-//       // переместим в начало страницы
-//       window.scrollTo({
-//         top: 0,
-//         left: 0,
-//         behavior: 'smooth',
-//       });
-//     };
-//   },
-// };
-
-// btnUp.addEventListener();
